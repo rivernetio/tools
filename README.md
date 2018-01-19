236dff6ed2febb9751d69efcf7bb59d6071d6b1b
Meng Ding <dingmeng@gmail.com>
2018-01-19 01:23:39 +0000

    Enhance offline packaging experience

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-# tools to install ecp.
+离线安装步骤：
 
-## ```prepare_offline_deps.sh``` 
-This will bundle all ECP images and rpms into .tar.gz files to do an offline installation elsewhere.
+## 本地环境准备离线安装包 （需要有外网的机器，安装且启动了docker）
+
+1. 下载辅助工具：
+
+   ```
+   https://github.com/rivernetio/tools
+   ```
+
+2. 创建离线安装包：  
+   运行脚本:
+
+   ```
+   ./prepare_offline_package.sh
+   ```
+
+   在当前目录下产生 ```ansible-ecp.tar.gz```
+
+## 部署环境安装离线安装包
+1. 拷贝ansible-ecp到部署机，
+
+   ```
+   tar xvfz ansible-ecp.tar.gz
+   cd ansible-ecp/
+   ```
+
+2. 配置机器间免秘钥登录（所有机器，可以参考ansible-ecp/scripts/pre-install.sh）
+
+3. 安装ansible： 
+
+   ```
+   yum localinstall depends/rpm/ansible/*.rpm
+   ```
+
+4. 安装ecp：
+
+   ```
+   // inventory,添加机器信息
+   ansible-playbook -v install.yml
+   ```
+
+5. 验证系统是否正确安装：
+
+   ```
+   浏览器访问：  IP:31081
+   登录用户名：  admin
+   登录密码：   password
+   ```
+   
---
+++ prepare_offline_package.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# export SITE=Toronto
+
+git --version > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "You must install git and make sure to have connection to the internet"
+  exit 1
+fi
+
+echo "Cloning ansible-ecp repository ..."
+if [ ! -d `pwd`/"ansible-ecp" ]; then
+  git clone https://github.com/rivernetio/ansible-ecp
+  if [ $? -ne 0 ]; then
+    echo "Failed to clone the ansible-ecp repository"
+    exit 1
+  fi
+fi
+
+cd ansible-ecp
+
+echo "Prepare offline dependencies ..."
+mkdir -p depends && cd depends
+../scripts/prepare_offline_deps.sh
+
+echo "Modify global variables ..."
+cd ..
+sed -i 's#src_image_tars_dir:.*#src_image_tars_dir: \"{{ playbook_dir }}/depends/image_tars\"#g' group_vars/all.yml
+sed -i 's#src_rpms_dir:.*#src_rpms_dir: \"{{ playbook_dir }}/depends/rpm\"#g' group_vars/all.yml
+sed -i 's#ecp_local_charts_repo:.*#ecp_local_charts_repo: \"{{ playbook_dir }}/depends/charts/repo\"#g' group_vars/all.yml
+
+cd ..
+tar -cvzf ansible-ecp.tar.gz ansible-ecp
